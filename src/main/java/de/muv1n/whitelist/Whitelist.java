7f1ef0db3c9607720b9b2b0167183d828a07aa51
MuV1N1 <marvin.kleiner1910@gmail.com>
2026-06-22 23:05:10 +0200

    Initial commit

---
+++ src/main/java/de/muv1n/whitelist/Whitelist.java
@@ -0,0 +1,14 @@
+package de.muv1n.whitelist;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Whitelist extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
